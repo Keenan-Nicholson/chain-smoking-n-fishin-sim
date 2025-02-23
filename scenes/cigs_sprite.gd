88a8e3dcedb1eb6b4b996253be605d5617304802
Keenan Nicholson <keenanbnicholson@gmail.com>
2025-02-23 18:14:18 -0330

    hp bars and regen

--- scenes/cigs_sprite.gd
+++ scenes/cigs_sprite.gd
@@ -3,6 +3,10 @@ extends Sprite2D
 var original_position = Vector2.ZERO
 var original_scale = Vector2.ZERO
 var shake_intensity = 5.0
+var key_cooldown = 1.0
+var last_s_key_press_time = 0.0
+var last_d_key_press_time = 0.0
+
 @onready var drag = $"../Drag"
 @onready var sip = $"../Sip"
 
@@ -18,14 +22,23 @@ func _process(_delta: float) -> void:
 	
 func _input(_event: InputEvent) -> void:
 	# smoke
-	if Input.is_key_pressed(KEY_S):
-		scale.x = lerp(0,1,1)
-		scale.y = lerp(1,0,0)
-		drag.play()
+	if Input.is_key_pressed(KEY_S) and not Input.is_key_pressed(KEY_D):
+		var current_s_time = Time.get_ticks_msec() / 1000.0  # Get time in seconds
+		if current_s_time - last_s_key_press_time >= key_cooldown:
+			scale.x = lerp(0,1,5)
+			scale.y = lerp(1,0,0)
+			drag.play()
+			last_s_key_press_time = current_s_time
+
 	# sip
-	if Input.is_key_pressed(KEY_D):
-		scale.x = lerp(0,1,1)
-		scale.y = lerp(1,0,0)
-		sip.play()
+	if Input.is_key_pressed(KEY_D) and not Input.is_key_pressed(KEY_S):
+		var current_d_time = Time.get_ticks_msec() / 1000.0  # Get time in seconds
+		if current_d_time - last_d_key_press_time >= key_cooldown:
+			scale.x = lerp(0,1,5)
+			scale.y = lerp(1,0,0)
+			sip.play()
+			last_d_key_press_time = current_d_time
+
+			
 	if not (Input.is_key_pressed(KEY_D) or Input.is_key_pressed((KEY_S))):
 		scale = original_scale
--- scenes/fishin.gd
+++ scenes/fishin.gd
@@ -1,7 +1,15 @@
 extends Node2D
-
+var smoke_hp = 100
+var drink_hp = 100
 var timer = Timer.new()
+var bar_speed = 10
+var key_cooldown = 1.0
+var last_s_key_press_time = 0.0
+var last_d_key_press_time = 0.0
+
 @onready var cough = $CiggyCough
+@onready var smoke_hp_bar = $CanvasLayer/SmokeBar
+@onready var drink_hp_bar = $CanvasLayer/DrinkBar
 
 func _ready():
 	cough.play()
@@ -9,13 +17,46 @@ func _ready():
 	timer.timeout.connect(_on_timer_timeout)
 	start_timer()
 	
-func _process(_delta: float) -> void:
-	pass
+	smoke_hp_bar.value = smoke_hp
+	drink_hp_bar.value = drink_hp
 	
+func _process(delta: float) -> void:
+	smoke_hp -= bar_speed * delta
+	smoke_hp_bar.value = smoke_hp
+	
+	drink_hp -= bar_speed * delta
+	drink_hp_bar.value = drink_hp
+
+	update_bar_color(smoke_hp_bar, smoke_hp)
+	update_bar_color(drink_hp_bar, drink_hp)
+			
 func start_timer():
 	timer.wait_time = randi_range(10,30)
-	print(timer.get_wait_time())
 	timer.start()
 	
 func _on_timer_timeout():
 	cough.play()
+	
+func _input(_event: InputEvent) -> void:
+	if Input.is_key_pressed(KEY_S) and not Input.is_key_pressed(KEY_D):
+		var current_s_time = Time.get_ticks_msec() / 1000.0
+		if current_s_time - last_s_key_press_time >= key_cooldown:
+			if smoke_hp >= 80:
+				smoke_hp = 100
+			else:
+				smoke_hp += 20
+			last_s_key_press_time = current_s_time
+
+	if Input.is_key_pressed(KEY_D) and not Input.is_key_pressed(KEY_S):
+		var current_d_time = Time.get_ticks_msec() / 1000.0 
+		if current_d_time - last_d_key_press_time >= key_cooldown:
+			if drink_hp >= 80:
+				drink_hp = 100
+			else:
+				drink_hp += 20
+			last_d_key_press_time = current_d_time
+
+func update_bar_color(bar: ProgressBar, value: float) -> void:
+	var hp_ratio = clamp(value / 100.0, 0, 1)  # Normalize between 0 and 1
+	var color = Color(1.0 - hp_ratio, hp_ratio, 0)  # Red to green transition
+	bar.modulate = color
